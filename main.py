7c4a193c815f500d9dc64d53be78ed7db988cd93
PedroAlves1500 <pedro.alves@tailwindsw.com>
2023-08-15 10:05:21 -0300

    MOD: adminOnly decorator.

--- main.py
+++ main.py
@@ -50,6 +50,18 @@ gravatar = Gravatar(app,
                     use_ssl=False,
                     base_url=None)
 
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If id is not 1 then return abort with 403 error
+        if current_user.id != 1:
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
 
 
 @login_manager.user_loader
